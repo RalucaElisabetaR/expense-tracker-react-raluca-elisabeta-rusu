ccf42e514a7cbc4a559814c0a3065dc5bcc08f76
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-07-05 18:49:29 +0200

    provider component

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from 'react'
-
+import AppReducer from './AppReducer'
 //Initial state
 
 const initialState = {
@@ -13,3 +13,14 @@ const initialState = {
 // create Context
 
 export const GlobalContext = createContext(initialState)
+
+//Provider component
+
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState)
+    return (
+        <GlobalContext.Provider value={{ transactions: state.transactions }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
