9fd9b9eb8371544678b6b105b9287fe9dfdf4488
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-07-05 19:11:04 +0200

    wrap in GlobalProvider

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext)
+
     return (
         <>
             <h3>History</h3>
-            <ul className="list" id="list">
-                <li className="minus">
-                    Cash<span>-$400</span>
-                    <button className="delete-btn">x</button>
-                </li>
+            <ul className="list">
+                {transactions.map((transaction) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <li className="minus">
+                        {transaction.text}
+                        <span>-$400</span>
+                        <button className="delete-btn">x</button>
+                    </li>
+                ))}
             </ul>
         </>
     )
