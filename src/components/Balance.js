bbe491fb0bd17590c7bebcad5b6481613cf494ae
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-07-05 18:07:02 +0200

    AddTransaction component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export const Balance = () => {
-  return (
-    <>
-      <h4>Your Balance</h4>
-      <h1 id='balance'>$0.00</h1>
-    </>
-  )
+    return (
+        <>
+            <h4>Your Balance</h4>
+            <h1>$0.00</h1>
+        </>
+    )
 }
