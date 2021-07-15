2b52813ad930eb7b4bf44268aa94c78fc4fb7726
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-07-15 19:31:21 +0200

    create add + delete transaction controller

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -5,3 +5,19 @@
 exports.getTransactions = (rec, res, next) => {
     res.send('GET Transactions')
 }
+
+// @desc Add all Transaction
+// @route POST /api/v1/transactions
+// @access Public
+
+exports.addTransaction = (rec, res, next) => {
+    res.send('POST transaction')
+}
+
+// @desc Delete all Transaction
+// @route DELETE /api/v1/transactions/:id
+// @access Public
+
+exports.deleteTransaction = (rec, res, next) => {
+    res.send('DELETE Transaction')
+}
